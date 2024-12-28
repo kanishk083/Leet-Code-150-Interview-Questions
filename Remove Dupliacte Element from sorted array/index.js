1c387cc59cee890fe7676e4673be50d8888eb902
Kanishk Wagh <146994401+kanishk083@users.noreply.github.com>
2024-12-28 16:19:25 +0530

    Delete Remove Dupliacte Element from sorted array directory

--- Remove Dupliacte Element from sorted array/index.js
+++
@@ -1,9 +0,0 @@
-var removeDuplicates = function(nums) {
-    for(let i=0;i<nums.length;i++){
-        for(j=nums.length;j>=i+1;j--){
-            if(nums[i]==nums[j]){
-               nums.splice(i,1)
-            }
-        }
-    }
-};
