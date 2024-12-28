61ce57a5c8845825cf0d7404388190b9dc556b46
Kanishk Wagh <146994401+kanishk083@users.noreply.github.com>
2024-12-28 16:20:35 +0530

    Add files via upload

---
+++ Leet Code  150 Interview Question/Remove Dupliacte Element from sorted array/index.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        for(j=nums.length;j>=i+1;j--){
+            if(nums[i]==nums[j]){
+               nums.splice(i,1)
+            }
+        }
+    }
+};
