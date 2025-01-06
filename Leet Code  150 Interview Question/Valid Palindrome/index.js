aa2034ab4209573d4b86a5a795c0045276e5afd9
Kanishk Wagh <146994401+kanishk083@users.noreply.github.com>
2025-01-06 18:29:25 +0530

    Add files via upload

---
+++ Leet Code  150 Interview Question/Valid Palindrome/index.js
@@ -0,0 +1,18 @@
+const check = (s)=> {
+    let l=0;
+    let r= s.length-1;
+    while(l<=r){
+        if(s[l]===s[r]){
+            l++;
+            r--;
+        }else{
+            return false;
+        }
+    }
+    return true;
+ }
+var isPalindrome = function(s) {
+    const regex=/[^a-zA-Z0-9^]/g
+    const clean=s.replace(regex,'').toLowerCase()
+    return check(clean);
+};
